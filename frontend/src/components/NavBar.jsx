9a7c9739844e9bd58df15a639d751bc663ba1cd7
jameswu <jamesw0462@gmail.com>
2023-01-20 22:09:58 -0800

    Added navbar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/navBar.css';
+
+function NavBar() {
+  return (
+    <div>
+      <div className='navigation'>
+        <div className='nav'>
+          <NavLink to='/control' className='link'>
+            Control
+          </NavLink>
+
+          <NavLink to='/leaderboard' className='link'>
+            Leaderboard
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ frontend/src/pages/Control.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/control.css';
+
+function Control() {
+  return (
+    <div>
+      <h1>SUP</h1>
+    </div>
+  );
+}
+
+export default Control;
