e5a0a721aa356dd588437edb7598529bb40984c1
jameswu <jamesw0462@gmail.com>
2023-01-20 23:05:22 -0800

    Tweaked control buttons, finished rough control page

--- frontend/src/pages/Control.jsx
+++ frontend/src/pages/Control.jsx
@@ -4,12 +4,20 @@ import ControlBtn from '../components/ControlBtn';
 
 function Control() {
   return (
-    <div className='App control'>
-      <div>
-        <ControlBtn stateON={true} text={'ON'} />
-        <ControlBtn stateON={false} text={'OFF'} />
+    <div className='App'>
+      <h1>Welcome, FINGERER.</h1>
+
+      <div className='control'>
+        <div className='container'>
+          <ControlBtn stateON={true} text={'ON'} />
+          <ControlBtn stateON={false} text={'OFF'} />
+        </div>
+
+        <div className='container'>
+          <h2>Your energy usage:</h2>
+          <h3>*insert pie chart here*</h3>
+        </div>
       </div>
-      <h1>Welcome, NAME</h1>
     </div>
   );
 }
